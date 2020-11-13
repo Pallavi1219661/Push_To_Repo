5d16a6c9ec91da0c0625dc06ed315c91f0ecbed8
Pallavi <kashyappallavi884@gmail.com>
2020-11-13 10:01:49 +0530

    first commit

---
+++ Add.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Add{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int sum=0;
+		sum=n1+n2;
+		System.out.println(sum);}}
